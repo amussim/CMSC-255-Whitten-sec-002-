7678e99f1b50ae83e072b62019e996694f799fa6
amussim <56137146+amussim@users.noreply.github.com>
2020-08-19 13:46:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Aneesh Mussim");
+		System.out.println("Hello");
+		System.out.println("testing out if the program works");
+		System.out.println("8/19/2020");
+		System.out.println("CMSC 255 sec 002");
+		System.out.println();
+		}
+	}
+	
